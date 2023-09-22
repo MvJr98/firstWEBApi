c09b9cbc9fc02cc844313bac140174a3d8802669
MvJr98 <tnveloso@gmail.com>
2023-09-21 22:44:44 -0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(db.findUsers());
+});
+
+router.post('/', (request, response) => {
+  db.insertUser(request.body);
+  response.status(201).json(users);
+})
+
+router.put('/:id',(request, response) => {
+  const id = request.params.id;
+  const user = db.updateUser(id, request.body);
+  response.status(200).json(user);
+})
+
+router.delete('/:id', (request, response) => {
+  const id = request.params.id;
+  const user = db.deleteUser(id);
+  response.status(200).json(user);
+})
+
+module.exports = router;
